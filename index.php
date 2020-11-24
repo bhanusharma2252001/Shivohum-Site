42cd027c7c75fa86490ec233419486c204dd118c
Bhavnesh sharma <bhanusharma252001@gmail.com>
2020-11-24 17:17:49 +0530

    Version 1

---
+++ index.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            www.Hariom.com
+        </title>
+        <!--Meta tags-->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+
+<!-- Bootstrap  and main CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+<link rel="stylesheet" href="css/main.css">
+
+<!--Googlel fonts-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <!--font awesome CDN-->  
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"> 
+
+    </head>
+    <body>
+    <section class="navigation">
+ <!--Navbar-expand-lg class for expanding bar horizontal. 
+    add navbar-brand class to brand logo link -->
+ <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark" >
+    <!--Brand link -->
+    <img class="logo" src="images/11.jpeg"> 
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <!--nav items-->
+   <ul class="navbar-nav mr-auto">
+      <li class="nav-item active"><a class="nav-link" href="#">Home</a></li> 
+      <!-- Dropdown item   -->
+      <li class="nav-item dropdown">
+        <a  class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+Service
+</a>
+<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+    <a class="dropdown-item" href="#">Mental life</a>
+    <a class="dropdown-item" href="#">Physical life</a>
+    <div class="dropdown-divider"></div>
+    <a class="dropdown-item" href="#">Financial life</a>
+    <a class="dropdown-item" href="#">Spiritual life</a>
+</div>
+</li> 
+<!--Dropdown Item end-->
+    <li class="nav-item"><a class="nav-link" href="#">Gallery</a></li> 
+      <li class="nav-item"><a class="nav-link" href="#">Contact us</a></li> 
+      <li class="nav-item"><a class="nav-link" href="#">About us</a></li> 
+    </ul> 
+</div>
+<button class="btn btn-primary Join-btn">Join Now</button>
+</nav>
+</section>
+
+<!-- Title Section-->
+<div id="carousel" class="carousel slide carousel-fade" data-ride="carousel" data-interval="2000">
+     
+    <div class="carousel-inner">
+  <div class="carousel-item active">
+      <img src="images\0001.jpg" class="d-block w-100 carousel-img" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h1 class="carousel-heading">Welcome to Hariom</h1>
+        <p>Hariom provides a health portal which provides holistic health services based on natural therapies</p>
+        <button class="btn btn-primary Join-btn1">Join Now</button>   
+    </div>
+  </div>
+  <div class="carousel-item">
+    <img src="images\0032.jpg" class="d-block w-100 carousel-img" alt="...">
+    <div class="carousel-caption d-none d-md-block">
+         <h1>Objective</h1>
+        <p>To Strengthen the individual and society and inner trasnformation through yoga and spiritual growth.
+        </p>
+        <button class="btn btn-primary Join-btn1">join now</button>
+      </div>
+  </div>
+  <div class="carousel-item">
+    <img src="images\04.jpg" class="d-block w-100 carousel-img" alt="...">
+    <div class="carousel-caption d-none d-md-block">
+      
+        <h1>Services</h1>
+        <p>We provide training to empower Physical life, Social life, Menatal life and Spiritual life</p>
+        <button class="btn btn-primary Join-btn1">join now</button>
+      </div>
+    
+  </div>
+</div>
+<a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+ 
+ <section id="about">
+     <div class="container-fluid">
+         <marquee><p>To register online yoga class click here</p></marquee>
+         <div class="row">
+             <div class="col-md-6 ">
+                <div id="carousel-a" class="carousel slide" data-ride="carousel" data-interval="2000">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                      </ol>
+                    <div class="carousel-inner">
+                  <div class="carousel-item active">
+                      <img src="images\1.jpg" class="d-block w-100 carousel-img" alt="...">
+                      <div class="carousel-caption d-none d-md-block">
+                        <h5>First slide label</h5>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                      </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="images\2.jpg" class="d-block w-100 carousel-img" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Second slide label</h5>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                      </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="images\3.jpg" class="d-block w-100 carousel-img" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Third slide label</h5>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                      </div>
+                    
+                  </div>
+                </div>
+                <a class="carousel-control-prev" href="#carousel-a" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carousel-a" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div>
+
+                
+             </div>
+             <div class="col-md-6">
+                Omkar Arogyam is a health portal which provides Holistic Health Services based on Natural Therapies like Yoga Therapy, Naturopathy, acupressure, Marma Therapy, Pranic Healing etc. It also provides various health education based lectures and study materials and health tips in order to have a healthy lifestyle.
+<br>
+                Objectives:<br>
+                <ul>
+                    <li>
+                        To encourage the holistic health (physical, mental, social and spiritual health) and a natural healthy lifestyle among the people.
+                
+                    </li>
+                    <li>
+                        To provide a disease cure and prevention system based on natural therapies, having no side effects.
+                
+                    </li>
+                    <li>
+                        To ensure a holistic health based services.
+                
+                    </li>
+                    <li>
+                        To create awareness regarding health and healthy lifestyle among the people.t
+
+                    </li>
+                    <li>
+                        To provide a health based education and lectures.
+                    </li>
+                </ul>
+            </div>
+        </div>
+         </div>
+        </div>
+</section>
+  <section id="service">
+      <h2 class="service-h2">OUR SERVICE</h2>
+      <div class="container-fluid">
+      <div class="row">
+          <div class="col-lg-5  service-heading"><h3 class="s-h3"><i class="fas fa-heartbeat s-icon"></i>  Mental life</h3></div>
+          <div class="col-lg-5  service-heading"><h3 class="s-h3"><i class="fas fa-heartbeat s-icon"></i>  Physical life</h3></div>
+          <div class="col-lg-5  service-heading"><h3 class="s-h3"><i class="fas fa-heartbeat s-icon"></i>  Spiritual life</h3></div>
+          <div class="col-lg-5  service-heading"><h3 class="s-h3"><i class="fas fa-heartbeat s-icon"></i>  Social life</h3></div>
+       
+      </div>
+    </div>
+  </section>
+
+ 
+    <footer class="container-fluid" id="footer">
+        <div class="row">
+            <div class="col">
+<h5>Hariom</h5><br>
+Kanpur, UP India<br>
+Phone:8810474430<br><br>
+Email:bhanusharma252001@gmail.com<br>
+ <i class="fa fa-facebook-official icons"> </i>
+ <i class="fa fa-google icons"> </i>
+ <i class="	fa fa-youtube icons"> </i>
+ <i class="fa fa-whatsapp icons"> </i> 
+ <i class="fa fa-pinterest-square icons"> </i>
+<i class="fa fa-twitter icons"></i>
+<i class="fa fa-envelope icons"></i>
+            </div>
+            <div class="col">
+           <h4>Our service</h4> 
+            
+               <li>Mental life</li>
+               <li>Physical life</li>
+               <li>Spiritual life</li>
+               <li>Social life</li>
+               
+            </div>
+            </div>
+        
+      </footer>
+      <p class="copy"> ©copyright 2020 Hariom</p>
+
+    
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+
+
+
+
+ <!-- Optional JavaScript -->
+ <script src="https://kit.fontawesome.com/yourcode.js"></script>
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  
+    </body>
+</html>
